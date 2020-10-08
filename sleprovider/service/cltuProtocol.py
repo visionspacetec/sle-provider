16c65b0da814a4fd3dc62f09e9a4ce11e9f35b4e
milenko-s <starcikm@gmail.com>
2020-10-08 14:12:54 +0200

    Implemented CLTU status report invocation handler

--- sleprovider/service/cltuProtocol.py
+++ sleprovider/service/cltuProtocol.py
@@ -19,8 +19,15 @@ class CltuProtocol(CommonProtocol):
         self.add_handler('CltuStartInvocation', self._start_invocation_handler)
         self.add_handler('CltuStopInvocation', self._stop_invocation_handler)
         self.add_handler('CltuTransferDataInvocation', self._transfer_data_invocation_handler)
-        # self.add_handler('RafScheduleStatusReportInvocation', self._schedule_status_report_invocation_handler)
-        self._production_status = 'operational'
+        self.add_handler('CltuScheduleStatusReportInvocation', self._schedule_status_report_invocation_handler)
+        self._cltu_last_processed = None
+        self._cltu_last_ok = None
+        self._cltu_production_status = 'operational'
+        self._uplink_status = 'nominal'
+        self._cltu_buffer_availiable = 4096  # ToDo: Read from service instance
+        self._number_of_cltus_received = 0
+        self._number_of_cltus_processed = 0
+        self._number_of_cltus_radiated = 0
         self._transfer_buffer = None
         self._release_timer = None
         self._report_timer = None
@@ -157,27 +164,139 @@ class CltuProtocol(CommonProtocol):
             else:
                 # ToDo: Implement produceNotification mechanism
                 raise NotImplementedError
+            self._number_of_cltus_received += 1
+            self.last_radiation_start_time = str(dt.datetime.utcnow())  # ToDo: How to find out when actually started?
+            str_time = dt.datetime.strptime(self.last_radiation_start_time, '%Y-%m-%d %H:%M:%S.%f')
+            time_days = (str_time - dt.datetime(1958, 1, 1)).days
+            time_ms = (str_time - dt.datetime(str_time.year, str_time.month, str_time.day)).seconds \
+                      * 1000 + ((str_time - dt.datetime(str_time.year, str_time.month,
+                                                        str_time.day)).microseconds // 1000)
+            time_micro = ((str_time -
+                           dt.datetime(str_time.year, str_time.month, str_time.day)).microseconds
+                          % 1000)
+            self.last_radiation_start_time = struct.pack('!HIH', time_days, time_ms, time_micro)
+            self._cltu_last_processed = self.cltu_identification  # ToDo: Get feedback from data_endpoint if actually radiated
+
             self.factory.container.data_endpoints[0].send_command('send-telecommand', [bytes(pdu['cltuData']).decode()])
+
+            self._number_of_cltus_processed += 1
+            self._number_of_cltus_radiated += 1  # ToDo: Get feedback from data_endpoint if actually radiated
+            self.last_radiation_stop_time = str(dt.datetime.utcnow())  # ToDo: How to find out when radiated?
+            str_time = dt.datetime.strptime(self.last_radiation_stop_time, '%Y-%m-%d %H:%M:%S.%f')
+            time_days = (str_time - dt.datetime(1958, 1, 1)).days
+            time_ms = (str_time - dt.datetime(str_time.year, str_time.month, str_time.day)).seconds \
+                      * 1000 + ((str_time - dt.datetime(str_time.year, str_time.month,
+                                                        str_time.day)).microseconds // 1000)
+            time_micro = ((str_time -
+                           dt.datetime(str_time.year, str_time.month, str_time.day)).microseconds
+                          % 1000)
+            self.last_radiation_stop_time = struct.pack('!HIH', time_days, time_ms, time_micro)
+            self._cltu_last_ok = self.cltu_identification  # ToDo: Get feedback from data_endpoint if actually radiated
 
             # ToDo: implement logic and different counting for return cltu identification if rejected
             pdu_return['cltuIdentification'] = self.cltu_identification + 1
-            # ToDo: Implement buffer size
-            pdu_return['cltuBufferAvailable'] = 2048
+            pdu_return['cltuBufferAvailable'] = self._cltu_buffer_availiable
             # ToDo: Negative result
             pdu_return['result']['positiveResult'] = None
             self._send_pdu(pdu_return)
 
+    def _schedule_status_report_invocation_handler(self, pdu):
+        logger.debug('Schedule Status Report Invocation received!')
+        pdu = pdu['cltuScheduleStatusReportInvocation']
+        pdu_return = CltuProviderToUserPdu()['cltuScheduleStatusReportReturn']
+        if 'used' in pdu['invokerCredentials']:
+            self._invoker_credentials = pdu['invokerCredentials']['used']
+            pdu_return['performerCredentials']['used'] = make_credentials(self.factory.container.local_id,
+                                                                          self.factory.container.local_password)
+        else:
+            self._invoker_credentials = None
+            pdu_return['performerCredentials']['unused'] = None
+        # ToDo: pdu_return['result']['negativeResult']['common'] = 'duplicateInvokeId'
+        pdu_return['invokeId'] = int(pdu['invokeId'])
+        if self.factory.container.si_config[self._inst_id]['state'] not in {'ready', 'active'}:
+            pdu_return['result']['negativeResult']['common'] = 'otherReason'
+        elif self.factory.container.remote_peers[self._initiator_id]['authentication_mode'] == \
+                'ALL' and not check_invoke_credentials(self._invoker_credentials,
+                                                       self._initiator_id,
+                                                       str(self.factory.container.remote_peers[
+                                                               str(self._initiator_id)]['password'])):
+            pdu_return['result']['negativeResult']['common'] = 'otherReason'
+        elif 'periodically' in pdu['reportRequestType']:
+            if not (self.factory.container.min_reporting_cycle
+                    <= int(pdu['reportRequestType']['periodically'])
+                    <= self.factory.container.max_reporting_cycle):
+                pdu_return['result']['negativeResult']['specific'] = 'invalidReportingCycle'
+            else:
+                if self._report_timer is None:
+                    self._report_timer = reactor.callLater(int(pdu['reportRequestType']['periodically']),
+                                                           self._send_status_report)
+                    self.factory.container.si_config[self._inst_id]['report_cycle'] = \
+                        int(pdu['reportRequestType']['periodically'])
+                else:
+                    self.factory.container.si_config[self._inst_id]['report_cycle'] = \
+                        int(pdu['reportRequestType']['periodically'])
+                    self._report_timer.reset(int(pdu['reportRequestType']['periodically']))
+                pdu_return['result']['positiveResult'] = None
+        elif 'stop' in pdu['reportRequestType']:
+            if self._report_timer is None:
+                pdu_return['result']['negativeResult']['specific'] = 'alreadyStopped'
+            else:
+                if (self._report_timer.called == 1) or (self._report_timer.cancelled == 1):
+                    self._report_timer = None
+                    pdu_return['result']['negativeResult']['specific'] = 'alreadyStopped'
+                else:
+                    self._report_timer.cancel()
+                    self._report_timer = None
+                    pdu_return['result']['positiveResult'] = None
+        elif 'immediately' in pdu['reportRequestType']:
+            self._send_status_report()
+            if self._report_timer is not None:
+                if (self._report_timer.called == 1) or (self._report_timer.cancelled == 1):
+                    self._report_timer = None
+                else:
+                    self._report_timer.cancel()
+                    self._report_timer = None
+            pdu_return['result']['positiveResult'] = None
+        else:
+            raise Exception()
+        self._send_pdu(pdu_return)
+
+    def _send_status_report(self):
+        if self.factory.container.si_config[self._inst_id]['state'] not in {'ready', 'active'}:
+            logger.error('Can not send status report in state: {}'
+                         .format(self.factory.container.si_config[self._inst_id]['state']))
+            return
+        pdu_invoc = CltuProviderToUserPdu()['cltuStatusReportInvocation']
+        if self.factory.container.remote_peers[self.factory.container.si_config[self._inst_id]['initiator_id']][
+                'authentication_mode'] == 'ALL':
+            pdu_invoc['invokerCredentials']['used'] = make_credentials(self.factory.container.local_id,
+                                                                       self.factory.container.local_password)
+        else:
+            pdu_invoc['invokerCredentials']['unused'] = None
+        if self._cltu_last_processed is None:
+            pdu_invoc['cltuLastProcessed']['noCltuProcessed'] = None
+        else:
+            pdu_invoc['cltuLastProcessed']['cltuProcessed']['cltuIdentification'] = self.cltu_identification
+            pdu_invoc['cltuLastProcessed']['cltuProcessed']['radiationStartTime']['known']['ccsdsFormat'] = \
+                self.last_radiation_start_time
+            pdu_invoc['cltuLastProcessed']['cltuProcessed']['cltuStatus'] = 'radiated'  # ToDo: Relate to something
+        if self._cltu_last_ok is None:
+            pdu_invoc['cltuLastOk']['noCltuOk'] = None
+        else:
+            pdu_invoc['cltuLastOk']['cltuOk']['cltuIdentification'] = self.cltu_identification
+            pdu_invoc['cltuLastOk']['cltuOk']['radiationStopTime']['ccsdsFormat'] = \
+                self.last_radiation_stop_time
+        pdu_invoc['cltuProductionStatus'] = self._cltu_production_status
+        pdu_invoc['uplinkStatus'] = self._uplink_status
+        pdu_invoc['numberOfCltusReceived'] = self._number_of_cltus_received
+        pdu_invoc['numberOfCltusProcessed'] = self._number_of_cltus_processed
+        pdu_invoc['numberOfCltusRadiated'] = self._number_of_cltus_radiated
+        pdu_invoc['cltuBufferAvailable'] = self._cltu_buffer_availiable
+        self._send_pdu(pdu_invoc)
+        if self._report_timer is not None:
+            if self._report_timer.called == 1:
+                self._report_timer = reactor.callLater(self.factory.container.si_config[self._inst_id]['report_cycle'],
+                                                       self._send_status_report)
+
     # def _get_parameter_invocation_handler(self, pdu):
-    #    pass
-
-    # def _schedule_status_report_invocation_handler(self, pdu):
-    #    pass
-
-    # def _send_status_report(self):
-    #    pass
-
-    # def append_to_transfer_buffer(self, frame_or_notification):
-    #    pass
-
-    # def _send_transfer_buffer(self):
-    #    pass
+    #     pass
